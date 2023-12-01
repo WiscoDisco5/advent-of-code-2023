1e0b392c5e252bd28d90dcc7e6f0625b91399798
John Goodwin <john.goodwin@ledgerinvesting.com>
2023-12-01 14:22:10 -0500

    Init and day 1 of advent of code

---
+++ 2023-12-01.py
@@ -0,0 +1,24 @@
+import re
+
+doc = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+"""
+
+
+def numbers(doc: str):
+    strings = doc.splitlines()
+    numbers = []
+    for string in strings:
+        num_str = re.sub("[^0-9]", "", string)
+        numbers.append(int(num_str[0] + num_str[-1]))
+    return sum(numbers)
+
+
+numbers(doc)
+
+with open("2023-12-01.txt") as file:
+    doc = file.read()
+
+numbers(doc)
