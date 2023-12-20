c968ba9f02a5a0ae78ed94c795fb1c2417485489
John Goodwin <john.goodwin@ledgerinvesting.com>
2023-12-20 13:53:58 -0600

    Sample working for day 18

--- 2023-12-18.py
+++ 2023-12-18.py
@@ -1,19 +1,4 @@
-sample = """R 6 (#70c710)
-D 5 (#0dc571)
-L 2 (#5713f0)
-D 2 (#d2c081)
-R 2 (#59c680)
-D 2 (#411b91)
-L 5 (#8ceee2)
-U 2 (#caa173)
-L 1 (#1b58a2)
-U 2 (#caa171)
-R 2 (#7807d2)
-U 3 (#a77fa3)
-L 2 (#015232)
-U 2 (#7a21e3)"""
 import copy
-from itertools import product
 
 
 def trace(doc: str):
@@ -21,8 +6,8 @@ def trace(doc: str):
     directions = [step.split(" ") for step in steps]
 
     # Set grid:
-    x = [0]
-    y = [0]
+    x = []
+    y = []
     curr_x = 0
     curr_y = 0
     for direction, length, _ in directions:
@@ -42,32 +27,33 @@ def trace(doc: str):
     lines = max(y) - min(y) + 1
     # # ("." * line_len + "\n") * lines
     grid = [["." for _ in range(line_len)] for _ in range(lines)]
-    # curr_x = 0 - min(x)
-    # curr_y = 0 - min(y)
-    # grid[curr_y][curr_x] = "#"
 
-    # # steps
-    # for x_step, y_step in zip(x, y):
-    #     if curr_x == x_step:
-    #         start = min(curr_y, y_step)
-    #         stop = max(curr_y, y_step)
-    #         for line_y in range(start, stop + 1):
-    #             grid[line_y][curr_x] = "#"
-    #     if curr_y == y_step:
-    #         start = min(curr_x, x_step)
-    #         stop = max(curr_x, x_step)
-    #         for line_x in range(start, stop):
-    #             grid[curr_y][line_x] = "#"
-    #     curr_y = copy.copy(y_step)
-    #     curr_x = copy.copy(x_step)
+    # Get outside shape
+    curr_x = 0 - min(x)
+    curr_y = 0 - min(y)
+    grid[curr_y][curr_x] = "#"
+    for x_step, y_step in zip(x, y):
+        if curr_x == x_step:
+            start = min(curr_y, y_step)
+            stop = max(curr_y, y_step)
+            for line_y in range(start, stop + 1):
+                grid[line_y][curr_x] = "#"
+        if curr_y == y_step:
+            start = min(curr_x, x_step)
+            stop = max(curr_x, x_step)
+            for line_x in range(start, stop):
+                grid[curr_y][line_x] = "#"
+        curr_y = copy.copy(y_step)
+        curr_x = copy.copy(x_step)
 
-    # product(range())
+    # Get inside shape
     polygon = [(x, y) for x, y in zip(x, y)]
     for x_pos in range(line_len):
         for y_pos in range(lines):
             if is_point_in_path(x_pos, y_pos, polygon):
                 grid[y_pos][x_pos] = "#"
-    return grid
+
+    return len([char for line in grid for char in line if char == "#"])
 
 
 def is_point_in_path(x: int, y: int, poly: list[tuple[int, int]]) -> bool:
@@ -100,4 +86,26 @@ def is_point_in_path(x: int, y: int, poly: list[tuple[int, int]]) -> bool:
     return c
 
 
-trace(sample)
+# Sample
+sample = """R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)"""
+
+assert trace(sample) == 62
+
+# Actual
+with open("2023-12-18.txt") as file:
+    actual = file.read()
+
+trace(actual)
